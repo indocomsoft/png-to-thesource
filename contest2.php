7432d68be8becad81523e7c1f6a973772aae644c
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2017-10-26 13:19:28 +0800

    Added optimisation of consecutive pixels with same colour horizontally

--- contest2.php
+++ contest2.php
@@ -1,133 +1,156 @@
 <?php
 
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+
+
 include_once("GDIndexedColorConverter.php");
+
+function convertcolour($current){
+	switch($current){
+		case 0x000000:
+		$code="b";
+		break;
+		
+		case 0xFFFFFF:
+		$code="a";
+		break;
+		
+		case 0x4BAE79:
+		$code="c";
+		break;
+		
+		case 0x3E50B4:
+		$code="d";
+		break;
+		
+		case 0xFE9700:
+		$code="e";
+		break;
+		
+		case 0xE81D62:
+		$code="f";
+		break;
+		
+		case 0xA900FE:
+		$code="g";
+		break;
+		
+		case 0xFD4235:
+		$code="h";
+		break;
+		
+		case 0xFEEA3A:
+		$code="i";
+		break;
+		
+		case 0x2095F2:
+		$code="j";
+		break;
+		
+		case 0x785447:
+		$code="k";
+		break;
+	}
+	return $code;
+}
 
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $imgType = exif_imagetype($image);
-	if($imgType != IMAGETYPE_PNG){
-		die("File uploaded is not png");
-	}
-}
-
 if (!move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 	die("Error in uploading");
 }
 
-$image = imagecreatefrompng($target_file);
+if(!($image = imagecreatefrompng($target_file))){
+	die("Image uploaded is not in png format.<br /><a href=\"javascript:window.history.back();\">Go back</a>");
+}
+
+if(!is_numeric($_POST["strength"])){
+	die("Dithering strength is not a number.<br /><a href=\"javascript:window.history.back();\">Go back</a>");
+}
+
+if($_POST["strength"] > 1.0 || $_POST["strength"] < 0.0){
+	die("Invalid dithering strength entered. Please enter a number between 0 and 1.<br /><a href=\"javascript:window.history.back();\">Go back</a>");
+}
 
 $converter = new GDIndexedColorConverter();
 
 $palette = array(
-		array(0x00,0x00,0x00),
-		array(0xFF,0xFF,0xFF),
-		array(0x4B,0xAE,0x79),
-		array(0x3E,0x50,0xB4),
-		array(0xFE,0x97,0x00),
-		array(0xE8,0x1D,0x62),
-		array(0xA9,0x00,0xFE),
-		array(0xFD,0x42,0x35),
-		array(0xFE,0xEA,0x3A),
-		array(0x20,0x95,0xF2),
-		array(0x78,0x54,0x47)
+		array(0x00,0x00,0x00),		//black
+		array(0xFF,0xFF,0xFF),		//white
+		array(0x4B,0xAE,0x79),		//green
+		array(0x3E,0x50,0xB4),		//indigo
+		array(0xFE,0x97,0x00),		//orange
+		array(0xE8,0x1D,0x62),		//pink
+		array(0xA9,0x00,0xFE),		//purple
+		array(0xFD,0x42,0x35),		//red
+		array(0xFE,0xEA,0x3A),		//yellow
+		array(0x20,0x95,0xF2),	//blue
+		array(0x78,0x54,0x47)		//brown
 );
 
-$new_image = $converter->convertToIndexedColor($image, $palette, 0.8);
+$new_image = $converter->convertToIndexedColor($image, $palette, $_POST["strength"]);
 
-imagepng($new_image, 'output.png', 0);
+$output_file = 'output'.basename($_FILES["fileToUpload"]["name"]).'.png';
 
-list($width, $height) = getimagesize('output.png');
-echo('
-function c(code){
-    if (code === 0){
-        return blank_bb;
-    } else if (code === 1){
-        return black_bb;
-    } else if (code === 2){
-        return green(black_bb);
-    } else if (code === 3){
-        return indigo(black_bb);
-    } else if (code === 4){
-        return orange(black_bb);
-    } else if (code === 5){
-        return pink(black_bb);
-    } else if (code === 6){
-        return purple(black_bb);
-    } else if (code === 7){
-        return red(black_bb);
-    } else if (code === 8){
-        return yellow(black_bb);
-    } else if (code === 9){
-        return blue(black_bb);
-    } else {
-        return brown(black_bb);
-    }
-}
+imagepng($new_image, $output_file, 0);
 
-function julius_2d_contest_0(){
-return ');
-
+list($width, $height) = getimagesize($output_file);
+$output = '<textarea style="height:100%; width:100%;">
+function draw_pic(){
+	var a = blank_bb;
+	var b = black_bb;
+	var c = green(black_bb);
+	var d = indigo(black_bb);
+	var e = orange(black_bb);
+	var f = pink(black_bb);
+	var g = purple(black_bb);
+	var h = red(black_bb);
+	var i = yellow(black_bb);
+	var j = blue(black_bb);
+	var k = brown(black_bb);
+	var l = stack_frac;
+	var m = quarter_turn_left;
+	return ';
+$count=1;
+$close_count=0;
+$block=0;
 
 for($i=0;$i<$height;$i++){
-	if($i<($height-1))echo("stack_frac(1/".($height-$i).",quarter_turn_left(");
-	else echo "quarter_turn_left(";
+	if($i<($height-1))$output = $output."l(1/".($height-$i).",m(";
+	else $output = $output."m(";
+	$close_count=0;
 	for($j=0;$j<$width;$j++){
-		$current=imagecolorat($new_image,$j,$i);
-		switch($current){
-			case 0x000000:
-			$code=1;
-			break;
-			
-			case 0xFFFFFF:
-			$code=0;
-			break;
-			
-			case 0x4BAE79:
-			$code=2;
-			break;
-			
-			case 0x3E50B4:
-			$code=3;
-			break;
-			
-			case 0xFE9700:
-			$code=4;
-			break;
-			
-			case 0xE81D62:
-			$code=5;
-			break;
-			
-			case 0xA900FE:
-			$code=6;
-			break;
-			
-			case 0xFD4235:
-			$code=7;
-			break;
-			
-			case 0xFEEA3A:
-			$code=8;
-			break;
-			
-			case 0x2095F2:
-			$code=9;
-			break;
-			
-			case 0x785447:
-			$code=10;
-			break;
+		if($j<($width-1)){
+			if(imagecolorat($new_image,$j,$i) == imagecolorat($new_image,$j+1,$i)){
+				if($count==1){
+					$ccurrent=imagecolorat($new_image,$j,$i);
+					$count++;
+				} else {
+					$count++;
+				}
+			} else {
+				$output = $output."l(".$count."/".($width-$j+$count-1).",".convertcolour(imagecolorat($new_image,$j,$i)).",";
+				$block++;
+				$close_count++;
+				$count=1;
+			}
+		} else {
+			if($count==1){
+				$output = $output."".convertcolour(imagecolorat($new_image,$j,$i))."";
+				$block++;
+			} else {
+				$output = $output."".convertcolour($ccurrent)."";
+				$block++;
+				$count=1;
+			}
 		}
-		if($j<($width-1))echo "stack_frac(1/".($width-$j).",c(".$code."),";
-		else echo "c(".$code.")";
 	}
-	for($j=0;$j<($width-1);$j++)echo ")";
-	echo ")";
-	if($i<($height-1))echo ",\n";
+	for($j=1;$j<=$close_count;$j++)$output = $output.")";
+	$output = $output.")";
+	if($i<($height-1))$output = $output.",";
 }
-for($i=0;$i<($height-1);$i++)echo ")";
-echo ";}";
+for($i=0;$i<($height-1);$i++)$output = $output.")";
+$output = $output.";}\n</textarea>";
+
+echo '<table border=0 height="100%" width="100%"><tr style="white-space:nowrap;"><td style="align:top;">'."No of blocks = ".$block."</td><td width=\"100%\" rowspan=\"2\">".$output."</td></tr><tr style=\"height:100%;vertical-align:top;\"><td><b>Preview:</b><br /><img src=\"".$output_file."\" width=\"256\" height=\"256\" style=\"border: 1px solid black; image-rendering: -moz-crisp-edges; image-rendering: -o-crisp-edges; image-rendering: -webkit-optimize-contrast;\" /><br />Run this on the interpreter:<br /><textarea>show(draw_pic());</textarea><br /><br /><a href=\"javascript:window.history.back();\">Go back</a></td></tr></table>";
